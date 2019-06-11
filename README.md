dcdf03f1b38bde1e020a83b69712a4c9ab0c1107
Gabriel Renan <xiubanido@gmail.com>
2019-06-11 16:46:49 -0300

    limpar

--- README.md
+++
@@ -1,1 +0,0 @@
-# interfaceONsystem
--- javascript/MODAL.js
+++
@@ -1,8 +0,0 @@
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  })
-
-  $(".alert").alert()
-  $('#myAlert').on('closed.bs.alert', function () {
-    // do something…
-  })
